344cdbbb2911c54ae2518eea6974ceda466a554d
Marcin Czapiewski <marcin.karol.czapiewski@gmail.com>
2017-11-04 21:34:38 +0100

    Initial commit.
    Already implemented some weight methods. Console app for now.

---
+++ src/com/programmingChallanges/chall000/WeightConverter.java
@@ -0,0 +1,115 @@
+package com.programmingChallanges.chall000;
+
+public class WeightConverter {
+
+    double gramsToDecagrams(double value) {
+        return value * 0.1;
+    }
+
+    double gramsToKilograms(double value) {
+        return value * 0.001;
+    }
+
+    double gramsToTones(double value) {
+        return value * 0.000001;
+    }
+
+    double gramsToPounds(double value) {
+         return value * 0.0022046226;
+    }
+
+    double gramsToOunces(double value) {
+        return value * 0.0352733686;
+    }
+
+    double gramsToCarats(double value) {
+        return value * 5.;
+    }
+
+    double gramsToGrain(double value) {
+        return value * 15.4320987654;
+    }
+
+    double gramsToHundredweightUS(double value) {
+        return value * 0.0000220462;
+    }
+
+    double gramsToHundredweightUK(double value) {
+        return value * 0.0000196841;
+    }
+
+    double decagramsToGrams(double value) {
+        return value * 10.;
+    }
+
+    double decagramsToKilograms(double value) {
+        return value * 0.01;
+    }
+
+    double decagramsToTones(double value) {
+        return value * 0.00001;
+    }
+
+    double decagramsToPounds(double value) {
+        return value * 0.0220462262;
+    }
+
+    double decagramsToOunces(double value) {
+        return value * 0.3527336861;
+    }
+
+    double decagramsToCarats(double value) {
+        return value * 50.;
+    }
+
+    double decagramsToGrain(double value) {
+        return value * 154.3209876543;
+    }
+
+    double decagramsToHundredweightUS(double value) {
+        return value * 0.0002204623;
+    }
+
+    double decagramsToHundredweightUK(double value) {
+        return value * 0.0001968413;
+    }
+
+    double kilogramsToGrams(double value) {
+        return value * 1000;
+    }
+
+    double kilogramsToDecagrams(double value) {
+        return value * 100;
+    }
+
+    double kilogramsToTones(double value) {
+        return value * 0.001;
+    }
+
+    double kilogramsToPounds(double value) {
+        return value * 2.2046226218;
+    }
+
+    double kilogramsToOunces(double value) {
+        return value * 35.2733686067;
+    }
+
+    double kilogramsToCarats(double value) {
+        return value * 5000.;
+    }
+
+    double kilogramsToGrain(double value) {
+        return value * 15432.0987654321;
+    }
+
+    double kilogramsToHundredweightUS(double value) {
+        return value * 0.0220462262;
+    }
+
+    double kilogramsToHundredweightUK(double value) {
+        return value * 0.0196841307;
+    }
+
+
+
+}
